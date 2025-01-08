3e1e2e6bf97d2c60b7b078f754acd6b817eb1e8c
Irfanuddin Shafi Ahmed <irfanudeen08@gmail.com>
2025-01-08 20:47:19 +0400

    chore: bump version

--- auth_util/refresh_token_pb2_grpc.py
+++ auth_util/refresh_token_pb2_grpc.py
@@ -3,7 +3,7 @@
 
 
 import grpc
-import refresh_token_pb2 as refresh__token__pb2
+import auth_util.refresh_token_pb2 as refresh__token__pb2
 
 GRPC_GENERATED_VERSION = "1.69.0"
 GRPC_VERSION = grpc.__version__
